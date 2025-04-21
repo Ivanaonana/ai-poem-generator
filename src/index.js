b6a3d8bb6c6c7be6e30afed33d9f791d1263caaa
Ivanaonana <wangyixing201709@outlook.com>
2025-04-21 18:28:19 -0400

    new changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 function displayPoem(response) {
-  console.log("Poem generated");
+  console.log("Generation Poem");
+  let formattedPoem = response.data.answer.replace(/\n/g, "<br>");
   new Typewriter("#poem", {
-    strings: response.data.answer,
+    strings: formattedPoem,
     autoStart: true,
     delay: 3,
     cursor: "|",
@@ -11,20 +12,24 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  // build the API URL
   let instructionsInput = document.querySelector("#instructions");
   let apiKey = "od09fc3b1tf767828aa0a0c334481a88";
-  let prompt = `User instructions: Generate a Magical Realism poem about ${instructionsInput.value}`;
   let context =
-    "You are a Magical Realist poem expert and love to write short poems. Your mission is to generate an 8-line poem with each line separated by a newline character (\\n). Make sure to follow the user instructions. Sign the poem.";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    "You are a poet specializing in writing 4 line short poems in Magical Realism in poem format. Make sure to break lines and follow user instructions. You only write the poem. You can sign if you want";
+  let prompt = `Generate a 4-line Magical Realist poem about ${instructionsInput.value} with each line separated by a newline character (\n).`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
+    prompt
+  )}&context=${encodeURIComponent(context)}&key=${encodeURIComponent(apiKey)}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating...</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
 
-  // Fetch the poem from the API
-  axios.get(apiUrl).then(displayPoem);
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-form");
